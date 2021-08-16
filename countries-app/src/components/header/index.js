73dc39cbbed6d533baf5ed96866fa00bd9b398ad
Marina Alber <marina.alber@itworx.com>
2021-08-16 06:02:26 +0200

    enhancing header

--- countries-app/src/components/header/index.js
+++ countries-app/src/components/header/index.js
@@ -1,20 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  faSun } from "@fortawesome/free-solid-svg-icons";
-import {  faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
 
-import "./index.scss";
+import "./styles.scss";
 
 function Header({ toggleTheme, isDark }) {
   return (
     <header className="header">
-          <div className="header__title">Where in the world?</div>
-          <div className="header__theme-toggle">
-            <button className="header__theme-btn btn" onClick={toggleTheme} title="Turn on">
-              <FontAwesomeIcon className="header__theme-icon" icon={isDark ? faSun : faMoon} />
-              {isDark ? "Light Mode" : "Dark Mode"}
-            </button>
-          </div>
-      
+      <div className="container">
+        <div className="header__title">Where in the world?</div>
+        <div className="header__theme-toggle">
+          <button
+            className="header__theme-btn btn"
+            onClick={toggleTheme}
+            title="Turn on"
+          >
+            <FontAwesomeIcon
+              className="header__theme-icon"
+              icon={isDark ? faSun : faMoon}
+            />
+            {isDark ? "Light Mode" : "Dark Mode"}
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
