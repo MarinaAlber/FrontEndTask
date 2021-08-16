a8b456eb1ccd6740e8b80a3df0653dd2fbfc3dc4
Marina Alber <marina.alber@itworx.com>
2021-08-16 06:05:27 +0200

    adding country card component

---
+++ countries-app/src/components/CountryCard/index.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "./styles.scss";
+
+function CountryCard(props) {
+  const { item } = props;
+  const { flag, name, population, capital, region } = item;
+  return (
+    <article className="country-card card shadow-sm ">
+      <img src={flag} className="card-img-top country-card__flag" alt={name} />
+      <div className="country-card__body">
+        <Link to={`details/${name}`}  className ="country-card__title font--bold">
+          <p >{name}</p>
+        </Link>
+        <div className="country-card__info">
+          <span className="font--regular">Population:</span>
+          <span className="font--light"> {population}</span>
+        </div>
+        <div class="country-card__info">
+          <span className="font--regular">Region:</span>
+          <span className="font--light"> {region}</span>
+        </div>
+        <div class="country-card__info">
+          <span className="font--regular">Capital:</span>
+          <span className="font--light"> {capital}</span>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default CountryCard;
